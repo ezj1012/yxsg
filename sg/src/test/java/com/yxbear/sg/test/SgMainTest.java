1aa104516c3595352af3ea1e595ebaf7036e45fd
熊剑 <ezj1012@qq.com>
2025-06-13 10:57:28 +0800

    feat :abc

--- sg/src/test/java/com/yxbear/sg/test/SgMainTest.java
+++ sg/src/test/java/com/yxbear/sg/test/SgMainTest.java
@@ -5,6 +5,7 @@ import com.yxbear.sg.SgAppMain;
 public class SgMainTest {
 
     public static void main(String[] args) {
+        args = new String[] {"--spring.profiles.active=code"};
         SgAppMain.main(args);
     }
 
